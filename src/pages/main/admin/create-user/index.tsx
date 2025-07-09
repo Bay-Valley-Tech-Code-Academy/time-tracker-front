280764f201bfdba79c44469bf63502c04851b14e
Sandy Wong <sandywongbvt2024@gmail.com>
2025-07-08 20:43:31 -0700

    created the user page and user table

--- src/pages/main/admin/create-user/index.tsx
+++ src/pages/main/admin/create-user/index.tsx
@@ -2,6 +2,23 @@ const CreateUserPage = () => {
   return (
     <div>
       <h1>Create User Page</h1>
+      <dialog id="formDialog">
+        <form method="dialog">
+          <div>
+            <label for="name">Name</label>
+            <input type="text" id="name" name="name" required></input>
+          </div>
+          <div>
+            <label for="email">Email</label>
+            <input type="email" id="email" name="email" required></input>
+          </div>
+          <button type="submit">Submit</button>
+        </form>
+      </dialog>
+
+      <button onclick="document.getElementById('formDialog').showModal()">Create User</button>
+      
+      <UserTable />
     </div>
   );
 };
