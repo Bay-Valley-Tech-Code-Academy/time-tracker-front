d3caa84de4882298073831d5e479733c126b7615
Sandy Wong <sandywongbvt2024@gmail.com>
2025-07-09 19:01:13 -0700

    Changed the user page

--- src/pages/main/admin/create-user/index.tsx
+++ src/pages/main/admin/create-user/index.tsx
@@ -1,22 +1,27 @@
+import UserTable from "./user-table";
+
 const CreateUserPage = () => {
+  function toggleDialog(){
+    
+  }
   return (
     <div>
       <h1>Create User Page</h1>
       <dialog id="formDialog">
         <form method="dialog">
           <div>
-            <label for="name">Name</label>
+            <label htmlFor="name">Name</label>
             <input type="text" id="name" name="name" required></input>
           </div>
           <div>
-            <label for="email">Email</label>
+            <label htmlFor="email">Email</label>
             <input type="email" id="email" name="email" required></input>
           </div>
           <button type="submit">Submit</button>
         </form>
       </dialog>
 
-      <button onclick="document.getElementById('formDialog').showModal()">Create User</button>
+      <button type="button" onClick={toggleDialog}>Create User</button>
       
       <UserTable />
     </div>
