c390585cc80c67fd840eba9d3e9ff33f3c09dc81
TriBellCat <56459622+TriBellCat@users.noreply.github.com>
2025-07-22 18:54:57 -0700

    Added in projects page

--- src/pages/main/admin/projects/index.tsx
+++ src/pages/main/admin/projects/index.tsx
@@ -1,10 +1,51 @@
+import React from "react";
 
 const ProjectsPage = () => {
-  return (
-    <div>
-      <h1>Projects Page</h1>
-    </div>
-  );
-};
+    const dialogRef = React.useRef<HTMLDialogElement>(null);
 
+    function showDialog() {
+        dialogRef.current?.showModal();
+    }
+
+    function closeDialog() {
+        dialogRef.current?.close();
+    }
+
+    return (
+        <div>
+            <button onClick={showDialog}>Create</button>
+            <dialog ref={dialogRef}>
+                <form>
+                    <label htmlFor="pname">Project Name:</label>
+                    <input
+                        type="text"
+                        id="pname"
+                        name="pname">
+                    </input>
+                    <button>Add Project</button>
+                    <button onClick={closeDialog}>Close</button>
+                </form>
+            </dialog>
+
+            <h2>Current Projects</h2>
+            <table>
+                <thead>
+                    <tr>
+                        <th>Project Name</th>
+                        <th>Total Hours</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {/* These are placeholders and have to be changed later */}
+                    <tr>
+                        <td>Placeholder</td>
+                        <td>1000</td>
+                    </tr>
+                </tbody>
+            </table>
+
+        </div>
+    )
+
+};
 export default ProjectsPage;
