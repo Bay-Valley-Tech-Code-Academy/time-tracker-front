c931aab58f13f933d0f51a96bc9f27105e6a7968
RRivera9 <55151480+RRivera9@users.noreply.github.com>
2025-09-02 10:54:42 -0700

    Functioning sessions

--- backend/routes/timeEntry.routes.js
+++ backend/routes/timeEntry.routes.js
@@ -1,20 +1,19 @@
 const express = require("express");
 const TimeEntry = require("../models/timeEntry.model");
 const router = express.Router();
+const { protect } = require("../middleware/authMiddleware");
 
-// GET /api/entries
-router.get("/", async (req, res) => {
+router.get("/", protect, async (req, res) => {
   try {
-    const { userId } = req.query; // optional filter
-    const query = userId ? { userId } : {};
-    const entries = await TimeEntry.find(query);
+    const entries = await TimeEntry.find({ userId: req.user._id });
     res.json(entries);
   } catch (err) {
     res.status(500).json({ error: err.message });
   }
 });
 
-router.post("/", async (req, res) => {
+const Test_User_ID = "68752a89652be6a1c8cf95e8";
+router.post("/", protect, async (req, res) => {
   try {
     const { summary, project, date, startTime, endTime } = req.body;
 
@@ -24,7 +23,15 @@ router.post("/", async (req, res) => {
       date,
       startTime,
       endTime,
+      userId: req.user._id || Test_User_ID
     });
+
+    //Delete later
+    if (!req.user) return res.status(401).json({ message: "Unauthorized" });
+
+    //Delete later
+    console.log('Request body:', req.body);
+    console.log('Authenticated user:', req.user);
 
     const savedEntry = await newEntry.save();
     res.status(201).json(savedEntry);
@@ -33,12 +40,18 @@ router.post("/", async (req, res) => {
   }
 });
 
-router.delete("/:id", async (req, res) => {
+router.delete("/:id", protect, async (req, res) => {
   try {
     const { id } = req.params;
     const deleted = await TimeEntry.findByIdAndDelete(id);
 
     if (!deleted) return res.status(404).json({ message: "Entry not found" });
+
+    console.log("checking authorization", deleted.userId.toString(), req.user._id.toString())
+
+    if (deleted.userId.toString() !== req.user._id.toString()) {
+      return res.status(403).json({ message: "Not authorized to delete this entry" });
+    }
 
     res.json({ message: "Entry deleted", id });
   } catch (err) {
